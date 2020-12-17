e54e154eaf19657ea29b63134088c528b2484112
muhammad <muhammadrehman21@gmail.com>
2020-12-16 22:03:13 -0500

    added the styling for the signup page

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react'
 import { withRouter } from 'react-router'
 import app from '../firebase'
+import './SignUp.scss'
 
 const SignUp = ({ history }) => {
   const handleSignUp = useCallback(
@@ -21,17 +22,25 @@ const SignUp = ({ history }) => {
 
   return (
     <div className='login'>
-      <h1>Sign up</h1>
       <form onSubmit={handleSignUp} className='login__form'>
-        <label>
-          Email
-          <input name='email' type='email' placeholder='Email' />
-        </label>
-        <label>
-          Password
-          <input name='password' type='password' placeholder='Password' />
-        </label>
-        <button type='submit'>Sign Up</button>
+        <h1 className='login__title'>Sign up</h1>
+        <label className='label'>Email</label>
+        <input
+          name='email'
+          type='email'
+          placeholder='Email'
+          className='label__input'
+        />
+        <label className='label'>Password</label>
+        <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          className='label__input'
+        />
+        <button type='submit' className='button__login'>
+          Sign Up
+        </button>
       </form>
     </div>
   )
